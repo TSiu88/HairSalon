2be6e3eb3f1ec6a2cf9f81c44ceb5810f162d55c
Tiffany Siu <tsiu88@gmail.com>
2020-03-20 14:47:30 -0700

    Try to limit phone number digits

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -4,7 +4,6 @@ namespace HairSalon.Models
   {
     public int ClientId { get; set; }
     public string ClientName { get; set; }
-
     public string PhoneNumber { get; set; }
     public int StylistId { get; set; }
     public virtual Stylist Stylist { get; set; }
